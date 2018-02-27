44150674f4574a46c8860fad736186688e68f82c
chenshigang <chenshigang@meituan.com>
2018-02-27 16:14:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
+const plugins = []
 
 module.exports = {
-  entry: {},
-  output: {},
-  resolve: [],
-  module: {},
-  plugins: {} 
+  entry: {
+    async: path.join(__dirname, './async.js')
+  },
+  output: {
+    filename: '[name].test.js',
+    path: __dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["@babel/preset-env"],
+            ["@babel/preset-react", {}],
+          ],
+          plugins: plugins.concat([
+            'syntax-dynamic-import',
+            'transform-class-properties',
+            'transform-decorators-legacy',
+            'transform-object-rest-spread'
+          ].map(x => require.resolve(`babel-plugin-${x}`)))
+        }
+      }
+    ]
+  }
 }
